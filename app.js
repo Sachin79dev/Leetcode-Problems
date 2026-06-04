3ced7153825d4b76db26c4653d01a878c165ba94
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-06-04 19:01:08 +0530

    2620. Counter
    Implement a createCounter function that returns a counter function.

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 
+
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+
+// 2620. Counter
+var createCounter = function(n) {
+    
+    return function() {
+        return n++
+    };
+};
