3d2f898826d8cbba57fdaad7b67c5d4d1e4ae533
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-09-21 20:42:17 +0530

    Add map function for array transformation
    Implement a map function to apply a callback to each element of an array.

--- app.js
+++ app.js
@@ -70,3 +70,34 @@ var createCounter = function(init) {
         }
     }
 };
+
+
+
+
+
+
+
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let result = []
+
+    for (i = 0; i < arr.length; i++) {
+        result.push(fn(arr[i], i));
+    }
+
+    return result;
+};
+
+
+
+
+
+
+
+
+
