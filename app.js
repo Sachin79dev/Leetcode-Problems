f8a3f164a022d1f64b1eaaebca55f59cf951e8d3
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-09-23 22:49:44 +0530

    Add reduce function for array accumulation
    Implement reduce function to accumulate values in an array.

--- app.js
+++ app.js
@@ -125,3 +125,24 @@ var filter = function(arr, fn) {
 
 
 
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let accum = init;
+    for (i = 0; i < nums.length; i++) {
+        accum = fn(accum, nums[i])
+    }
+    return accum;
+};
+
+
+
+
+
+
+
+
